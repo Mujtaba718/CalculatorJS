565e18d72a5cb050bf97edb09c69eb37d55ec57a
Mujtaba Iqbal <mujtaba.iq@gmail.com>
2018-03-08 20:44:08 -0500

    Cleaned up javascript

--- js/app.js
+++ js/app.js
@@ -1,27 +1,25 @@
+const buttons = document.getElementsByTagName("button");
+
+
 calculator.addEventListener("click", operation);
 
 function operation(e){
-	var inputField = document.getElementById("display")
+	var inputField = document.getElementById("display");
 
 	if(e.target.value !== "=" && e.target.value !== "C" && e.target.value !== "Del" && e.target.value !== "+/-"){
 		inputField.value += e.target.value;
-	}
-
-	if(e.target.value === "Del"){
+	} else if(e.target.value === "Del"){
 		inputField.value = inputField.value.substring(0, inputField.value.length - 1)
-	}
-
-	if(e.target.value === "="){
+	} else if(e.target.value === "="){
 		inputField.value = eval(inputField.value)
-	}
-
-	if(e.target.value === "C"){
+	} else if(e.target.value === "C"){
 		inputField.value = "";
-	}
-
-	if(e.target.value === "+/-"){
+	} else if(e.target.value === "+/-"){
 		inputField.value = "-" + inputField.value;
 	}
-
 }
+
+
+
+
 
